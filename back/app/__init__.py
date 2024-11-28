f4fb7fd86460a27ba7174d05492d363076a42211
MASINA Elliot <elliot.masina@epitech.eu>
2024-11-28 16:06:36 +0100

    Fix: add middleware cors for cross origin and OPTION method

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -3,6 +3,7 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
 
@@ -18,6 +19,15 @@ def get_config():
     return Settings()
 
 app = FastAPI()
+
+# Configuration du middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],  # Autorise les headers
+)
 
 def create_app(config_class=Config):
     init_app(app)
