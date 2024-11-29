8b62f206d74bd3d62832e91c73b66b0bb99ae1d6
Astios <mail.astios@gmail.com>
2024-11-29 16:56:23 +0100

    feat: better user managment

--- back/app/database/userDao.py
+++ back/app/database/userDao.py
@@ -9,9 +9,13 @@ def get_database(config_class=Config):
 
 class UserDAO:
     @staticmethod
-    async def find_user_by_username(username):
-        return await get_database().users.find_one({"username": username})
+    async def insert_user(email, hashed_password):
+        return await get_database().users.insert_one({"email": email, "password": hashed_password})
 
     @staticmethod
-    async def insert_user(username, hashed_password):
-        return await get_database().users.insert_one({"username": username, "password": hashed_password})
+    async def find_user_by_email(email):
+        return await get_database().users.find_one({"email": email})
+
+    @staticmethod
+    async def delete_user_by_email(email):
+        return await get_database().users.delete_one({"email": email})
