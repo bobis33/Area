561ba4ace2549b008fb822fe49a2c225e0743ccb
Asti0s <mail.astios@gmail.com>
2024-12-03 20:41:07 +0100

    feat: area DAO

--- back/app/database/Dao.py
+++ back/app/database/Dao.py
@@ -7,7 +7,8 @@ def get_database(config_class=Config):
 
     return db
 
-class UserDAO:
+class DAO:
+    # User management
     @staticmethod
     async def insert_user(email, hashed_password):
         return await get_database().users.insert_one({"email": email, "password": hashed_password})
@@ -19,3 +20,17 @@ class UserDAO:
     @staticmethod
     async def delete_user_by_email(email):
         return await get_database().users.delete_one({"email": email})
+
+
+    # Areas
+    @staticmethod
+    async def insert_area(area):
+        return await get_database().areas.insert_one(area)
+
+    @staticmethod
+    async def find_area_by_uuid(uuid):
+        return await get_database().areas.find_one({"uuid": uuid})
+
+    @staticmethod
+    async def delete_area_by_uuid(uuid):
+        return await get_database().areas.delete_one({"uuid": uuid})
