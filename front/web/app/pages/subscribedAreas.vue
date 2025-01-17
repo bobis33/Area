0770a72fba98d33a7e8125257a7dc9a57f9841d7
YohanDecamps <yohan.decamps@epitech.eu>
2025-01-18 00:44:10 +0100

    feat: reworked the front web style for my areas page

--- front/web/app/pages/subscribedAreas.vue
+++ front/web/app/pages/subscribedAreas.vue
@@ -1,39 +1,46 @@
 <template>
-  <div>
-    <h2>{{$t('subscribedArea')}}</h2>
-    <div>
-      <button @click="fetchSubscribedAreas">{{$t('fetchSubscribedAreas')}}</button>
+  <section class="hero is-fullheight" style="background-color: #272727;">
+    <div class="container">
+      <div class="columns is-vcentered" style="padding-top: 5%; padding-bottom: 2%; color: white;">
+        <div class="column is-4"></div>
+        <div class="column is-4 has-text-centered">
+          <h1 class="title">{{ $t('My AREAS') }}</h1>
+        </div>
+        <div class="column is-4 has-text-right">
+          <div style="display: flex; justify-content: flex-end; margin-right: 10%;">
+            <div style="display: flex; align-items: center; margin-right: 20px;">
+              <nuxt-link to="/areas" class="link-button" style="color: white;">{{$t('Shared Areas')}}</nuxt-link>
+            </div>
+            <div style="display: flex; align-items: center;">
+              <nuxt-link to="/createAreas" class="link-button" style="color: white;">{{$t('Create')}}</nuxt-link>
+            </div>
+            <nuxt-link to="/profile" class="link-button" style="color: white;">
+              <img src="@/assets/icons/account.png" alt="Plus Icon" style="width: 30; height: 30px; filter: invert(1); margin: 3px; padding-left: 20px;"/>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div v-if="filteredAreas.length" class="areas-container" style="padding-top: 2%">
+        <ul class="columns is-multiline">
+          <li v-for="area in filteredAreas" :key="area._id" class="column is-one-quarter">
+            <div class="card is-flex is-flex-direction-column is-justify-content-space-between" style="height: 100%; background-color: #000000; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);">
+              <div class="has-text-centered" style="padding-bottom: 5%; padding-top: 5%;">
+                <strong>{{ area.action }}</strong> <br>
+                <strong>{{ area.reaction }}</strong>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <p class="notification is-warning">No subscribed areas found.</p>
+      </div>
     </div>
-    <div v-if="subscribedAreas.length">
-      <ul>
-        <li v-for="area in subscribedAreas" :key="area._id" class="area-box">
-          <strong>{{ $t('action') }}:</strong> {{ area.action }} <br>
-          <strong>{{ $t('reaction') }}:</strong> {{ area.reaction }}
-          <strong>{{ $t('params action') }}:</strong> <br>
-          <ul>
-            <li v-for="(value, key) in area.action_params" :key="key">
-              <strong>{{ $t(key) }}:</strong> {{ value }}
-            </li>
-          </ul>
-          <strong>{{ $t('params reaction') }}:</strong> <br>
-          <ul>
-            <li v-for="(value, key) in area.reaction_params" :key="key">
-              <strong>{{ $t(key) }}:</strong> {{ value }}
-            </li>
-          </ul>
-          <button @click="unsubscribeUser(area._id)">{{$t('Unsubscribe')}}</button>
-        </li>
-      </ul>
-    </div>
-    <div v-else>
-      <p>No subscribed areas found.</p>
-    </div>
-
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useCookie } from '#app'
 import { CookiesEnum } from "~/config/constants";
 import { Areas } from '~/infrastructure/repositories/AreaRepository'
@@ -43,30 +50,14 @@ const config = useRuntimeConfig()
 
 interface Area {
   _id: string;
-  action: Action;
-  action_params: {},
-  reaction: Reaction;
-  reaction_params: {}
+  action: string;
+  action_params: Record<string, string>;
+  reaction: string;
+  reaction_params: Record<string, string>;
 }
 
-interface params {
-  [key: string]: string;
-}
 const subscribedAreas = ref<Area[]>([])
-
-interface Action {
-  name: string;
-  description: string;
-  service: string;
-  params: params;
-}
-
-interface Reaction {
-  name: string;
-  description: string;
-  service: string;
-  params: params;
-}
+const searchQuery = ref('')
 
 const fetchSubscribedAreas = async () => {
   try {
@@ -83,7 +74,7 @@ const fetchSubscribedAreas = async () => {
 const unsubscribeUser = async (area_id: string) => {
   try {
     if (token) {
-      const response = await new Areas().unsubscribeUser(area_id, token);
+      await new Areas().unsubscribeUser(area_id, token);
       await fetchSubscribedAreas()
     } else {
       console.error('Token is not available');
@@ -93,113 +84,21 @@ const unsubscribeUser = async (area_id: string) => {
   }
 }
 
-const getServiceClass = (service: string) => {
-  switch (service) {
-    case 'GMail':
-      return 'gmail-box';
-    case 'Github':
-      return 'github-box';
-    case 'Spotify':
-      return 'spotify-box';
-    default:
-      return '';
-  }
-}
+const filteredAreas = computed(() => {
+  return subscribedAreas.value.filter(area =>
+    area.action.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+    area.reaction.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
 
 onMounted(() => {
   fetchSubscribedAreas();
 });
-
 </script>
 
-<style scoped lang="scss">
-.actions-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-  flex-direction: column;
-  align-content: stretch;
-}
-
-.reactions-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-  flex-direction: column;
-  align-content: stretch;
-}
-
-.areas-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-  flex-direction: column;
-  align-content: stretch;
-}
-
-.flex-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(30%, 1fr)); /* Boxes are always 30% wide */
-  gap: 10px;
-  list-style-type: none;
-  padding: 0;
-}
-
-.area-box {
-  box-sizing: border-box; /* Includes padding and border in width calculation */
-  width: 100%;           /* Enforce full width within grid cell */
-  padding: 10px;
-  border: 1px solid #ccc;
-  text-align: left;
-  min-height: 200px;      /* Optional: Enforce consistent height */
-}
-
-input[type="text"] {
-  box-sizing: border-box;
-  width: 100%;
-  padding: 5px;
-  margin-top: 5px;
-  color: #000; /* Ensure text color is visible */
-  background-color: #fff; /* Ensure background color contrasts with text color */
-  border: 1px solid #ccc; /* Optional: Add border for better visibility */
-}
-
-.param-input {
-  color: #000; /* Ensure text color is visible */
-  background-color: #fff; /* Ensure background color contrasts with text color */
-  padding: 5px;
-  margin-top: 5px;
-  width: 100%;
-  border: 1px solid #ccc; /* Optional: Add border for better visibility */
-}
-
-.gmail-box {
-  background-color: #B02D2D;
-  color: white;
-}
-
-.github-box {
-  background-color: #3C2A3D;
-  color: white;
-}
-
-.spotify-box {
-  background-color: #1db954;
-  color: white;
-}
-
-  strong {
-    display: block;
-    margin-bottom: 8px;
-  }
-
-.action-box {
-  background-color: #007bff; /* Blue */
-  color: white;
-}
-
-.reaction-box {
-  background-color: #dc3545; /* Red */
-  color: white;
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
+html, body {
+  height: 100%;
 }
 </style>
